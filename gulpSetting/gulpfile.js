3db1e6f85a7a83189f2db6855eb6f82907f2704e
oinochoe <oinochoe@naver.com>
2019-01-01 16:15:15 +0900

    gulp server setting 및 live로 default 옵션 추가

--- gulpSetting/gulpfile.js
+++ gulpSetting/gulpfile.js
@@ -1,25 +1,49 @@
-var fileinclude = require('gulp-file-include'),
+var fileinclude = require("gulp-file-include"),
 	sass = require("gulp-sass"),
-	gulp = require('gulp');
+	connect = require("gulp-connect"),
+	gulp = require("gulp");
 
 // html include
-gulp.task('fileinclude', function(){
-	return gulp.src(['./app/src/index.html', './app/src/sub/*.html'], {base : './'})
+gulp.task("fileinclude", function(){
+	return gulp.src(["./app/src/index.html", "./app/src/sub/*.html"], {base : "./"})
 	.pipe(fileinclude({
-		prefix: '@@',
-		basepath: '@file'
+		prefix: "@@",
+		basepath: "@file"
 	}))
-	.pipe(gulp.dest('./'));
+	.pipe(gulp.dest("./"));
 });
 
 // sass
-gulp.task('sass', function(){
-	return gulp.src('./app/scssfile/*.scss')
-	.pipe(sass({outputStyle : 'compact'}).on('error', sass.logError))
-	.pipe(gulp.dest('./app/src/css'));
+gulp.task("sass", function(){
+	return gulp.src("./app/scssfile/*.scss")
+	.pipe(sass({outputStyle : "compact"}).on("error", sass.logError))
+	.pipe(gulp.dest("./app/src/css"));
 });
 
 // sass --watch
-gulp.task('sass:watch', function(){
-	gulp.watch('./app/scssfile/*.scss', gulp.series('sass'));
+gulp.task("sass:watch", function(){
+	gulp.watch("./app/scssfile/*.scss", gulp.series("sass"));
 });
+
+// gulp 서버
+gulp.task("connect", function(){
+	connect.server({
+		root: "./app/src",
+		livereload: true,
+		port: 8001
+	});
+});
+
+gulp.task("html", function(){
+	gulp.src("./app/src/*.html")
+	.pipe(connect.reload());
+});
+
+gulp.task("watch", function(){
+	gulp.watch("./app/src/*.html", gulp.series("html")); //html reload
+	gulp.watch("./app/scssfile/*.scss", gulp.series("sass")); // sass 자동 컴파일
+});
+
+gulp.task("live", gulp.series("connect", "watch", function(done){
+	done();
+}));
